1fa3631f7d8878dcd5ba455f749dd64dd5c2b19c
John <jgz6@uakron.edu>
2023-04-01 14:28:24 -0400

    created cli tool

--- pdfreader.py
+++ pdfreader.py
@@ -1,42 +1,42 @@
 from PyPDF2 import PdfReader
-#from gtts import gTTS
 import pyttsx3
 import os
+import argparse
 from tqdm import tqdm
 
-reader = PdfReader('book.pdf')
-finalText = ''
 
-dirname = 'my_folder'
-filename = 'myfile'
+def convert_pdf_to_mp3(input_path, output_path):
+    reader = PdfReader(input_path)
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
 
-#   os.makedirs(dirname)
-parts = []
-def visitor_body(text, cm, tm, fontDict, fontSize):
-    y = tm[5]
-    print(f"{y}: {text}")
-    if y < 600:
-        parts.append(text)
+    parts = []
+    def visitor_body(text, cm, tm, fontDict, fontSize):
+        y = tm[5]
+        #print(f"{y}: {text}")
+        if y < 600:
+            parts.append(text)
 
-counter = 0
-engine = pyttsx3.init()
-for i in tqdm(range(len(reader.pages))):
-    text = reader.pages[i].extract_text(visitor_text=visitor_body)
-    finalText += text
-    #print(parts)
-    if i > 14: 
-        print("".join(parts))
-        quit()
+    counter = 0
+    engine = pyttsx3.init()
+    for i in tqdm(range(len(reader.pages))):
+        text = reader.pages[i].extract_text(visitor_text=visitor_body)
+        if i % 30 == 0: 
+            #print("".join(parts))
+            file_path = os.path.join(output_path, f'{output_path}-{counter}.mp3')
+            engine.save_to_file("".join(parts), file_path)
+            counter += 1
+            engine.runAndWait()
+            parts = []
 
-"""    if i % 30 == 0:
-        file_path = os.path.join(dirname, f'{filename}-{counter}.mp3')
-        engine.save_to_file(finalText, file_path)
-        counter += 1
-        engine.runAndWait()"""
-
-#engine.setProperty('rate', 250)
-
-#engine.say(finalText)
-#engine.runAndWait()
-
-print('completed')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Convert a PDF to an MP3')
+    parser.add_argument('pdf_file', type=str, help='name of input pdf file')
+    parser.add_argument('mp3_file', type=str, help='name of file output')
+    args = parser.parse_args()
+    
+    # Convert PDF to MP3
+    convert_pdf_to_mp3(args.pdf_file, args.mp3_file)
+    
+    # Print success message
+    print(f'Successfully converted {args.pdf_file} to {args.mp3_file}')
