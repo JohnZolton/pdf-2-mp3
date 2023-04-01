49b536eb07a98cb8f992bbfa759ae6ea6eec1b57
John <jgz6@uakron.edu>
2023-04-01 13:43:31 -0400

    ignore headers

--- pdfreader.py
+++ pdfreader.py
@@ -1,21 +1,42 @@
 from PyPDF2 import PdfReader
 #from gtts import gTTS
 import pyttsx3
+import os
+from tqdm import tqdm
 
-reader = PdfReader('testing.pdf')
+reader = PdfReader('book.pdf')
 finalText = ''
 
-for page in reader.pages:
-    finalText += page.extract_text(0)
+dirname = 'my_folder'
+filename = 'myfile'
 
-#print(finalText)
+#   os.makedirs(dirname)
+parts = []
+def visitor_body(text, cm, tm, fontDict, fontSize):
+    y = tm[5]
+    print(f"{y}: {text}")
+    if y < 600:
+        parts.append(text)
 
+counter = 0
 engine = pyttsx3.init()
-engine.setProperty('rate', 250)
+for i in tqdm(range(len(reader.pages))):
+    text = reader.pages[i].extract_text(visitor_text=visitor_body)
+    finalText += text
+    #print(parts)
+    if i > 14: 
+        print("".join(parts))
+        quit()
+
+"""    if i % 30 == 0:
+        file_path = os.path.join(dirname, f'{filename}-{counter}.mp3')
+        engine.save_to_file(finalText, file_path)
+        counter += 1
+        engine.runAndWait()"""
+
+#engine.setProperty('rate', 250)
 
 #engine.say(finalText)
 #engine.runAndWait()
 
-engine.save_to_file(finalText, 'testing.mp3')
-engine.runAndWait()
 print('completed')
